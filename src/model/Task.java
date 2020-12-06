f7cbfaa6711d5a42b1ea60952bd670548c991829
Jack Hamer <jackhamer0990@gmail.com>
2020-12-06 16:28:41 +0100

    task and tasklist classes

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,4 +1,95 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Task {
+  private int id;
+  private String name;
+  private String description;
+  private int estimatedTime;
+  private int deadlineTime;
+  private TeamMember responsibleTeamMember;
+  private Status status;
+  private TrackTimeList trackTimeList;
+
+  public Task(String name, int id, int estimatedTime, String description, int deadlineTime) {
+    this.id=id;
+    this.name=name;
+    this.description=description;
+    this.deadlineTime=deadlineTime;
+    this.estimatedTime=estimatedTime;
+    this.status=new Status();
+  }
+  public Task(String name, int id, int estimatedTime, String description, int deadlineTime, TeamMember responsibleTeamMember) {
+    this.id=id;
+    this.name=name;
+    this.description=description;
+    this.deadlineTime=deadlineTime;
+    this.estimatedTime=estimatedTime;
+    this.responsibleTeamMember=responsibleTeamMember;
+    this.status=new Status();
+  }
+
+  public int getTaskID() {
+    return this.id;
+  }
+  /*public int getRequirementID() {
+    return super.get;
+  }*/
+  public String getName() {
+    return this.name;
+  }
+  public String getDescription() {
+    return this.description;
+  }
+  public int getEstimatedTime() {
+    return this.estimatedTime;
+  }
+  public int getDeadlineTime() {
+    return this.deadlineTime;
+  }
+  public int getTimeSpent() {
+    return this.trackTimeList.getTotaltime();
+  }
+  public int getTimeSpentOfMember(TeamMember teamMember) {
+    return this.trackTimeList.getTimeOfMember(teamMember);
+  }
+  public TeamMember[] getTeamMembers() {
+    return this.trackTimeList.getTeamMembers();
+  }
+  public TeamMember getResponsibleTeamMember() {
+    return this.responsibleTeamMember;
+  }
+  public Status getStatus() {
+    return this.status;
+  }
+  public TrackTime[] getTrackTime() {
+    return this.trackTimeList.getTrackTime();
+  }
+
+  public void setName(String name) {
+    this.name=name;
+  }
+  public void setDescription(String description) {
+    this.description=description;
+  }
+  public void setDeadlineTime(int time) {
+    this.deadlineTime=time;
+  }
+  public void setEstimatedTime(int estimatedTime) {
+    this.estimatedTime=estimatedTime;
+  }
+  public void setResponsibleTeamMember(TeamMember teamMember) {
+    this.responsibleTeamMember=teamMember;
+  }
+  public void setStatus(Status status) {
+    this.status=new Status(status);
+  }
+  public void setTimeWorked(TeamMember teamMember, int time) {
+    this.trackTimeList.setTimeWorked(teamMember, time);
+  }
+
+  public String toString() {
+    return "";
+  }
 }
--- src/model/TaskList.java
+++ src/model/TaskList.java
@@ -1,4 +1,69 @@
 package model;
 
+import java.util.ArrayList;
+
 public class TaskList {
+  private ArrayList<Task> tasks;
+  private int tasksCreated = 0;
+
+  public TaskList()
+  {
+    this.tasks = new ArrayList<Task>();
+  }
+
+  public void addTask(String name, int estimatedTime, String description, int deadlineTime)
+  {
+    tasks.add(new Task(name, this.tasksCreated, 0, description, deadlineTime));
+    this.tasksCreated++;
+  }
+  public void addTask(String name, int estimatedTime, String description, int deadlineTime, TeamMember responsibleTeamMember)
+  {
+    tasks.add(new Task(name, this.tasksCreated, 0, description, deadlineTime, responsibleTeamMember));
+    this.tasksCreated++;
+  }
+
+  public Task[] getAllTasks() {
+    return this.tasks.toArray(new Task[0]);
+  }
+  public Task[] getTasksByName(String name)
+  {
+    ArrayList<Task> foundTasks = new ArrayList<Task>();
+    for (Task task : this.tasks)
+    {
+      if (task.getName().equalsIgnoreCase(name)) {
+        foundTasks.add(task);
+      }
+    }
+    return foundTasks.toArray(new Task[0]);
+  }
+  public Task[] getRequirementsByStatus(Status status) {
+    ArrayList<Task> foundTasks = new ArrayList<Task>();
+    for (Task task : this.tasks)
+    {
+      if (task.getStatus().equals(status)) {
+        foundTasks.add(task);
+      }
+    }
+    return foundTasks.toArray(new Task[0]);
+  }
+  /*public Requirement[] getRequirementsBeforeDeadline(Status status) {
+    ArrayList<Requirement> foundRequirements = new ArrayList<Requirement>();
+    for (Requirement requirement : this.requirements)
+    {
+      if (requirement.getStatus().equals(status))
+        foundRequirements.add(requirement);
+    }
+    return foundRequirements.toArray(new Requirement[0]);
+  }*/
+  public int getUsedTime() {
+    int totalTime = 0;
+    for (Task task : this.tasks)
+    {
+      totalTime+=task.getTimeSpent();
+    }
+    return  totalTime;
+  }
+  public void deleteTask(Task task) {
+    this.tasks.remove(task);
+  }
 }
